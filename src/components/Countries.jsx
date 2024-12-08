c4faa3c495f39ba5535bcb48f05941d715f096bf
unknown <abir045@gmail.com>
2024-12-08 18:09:03 +0600

    updated effects with react-awesome-reveal

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -7,59 +7,66 @@ import india from "../assets/india.jpg";
 import uk from "../assets/uk.jpg";
 import aus from "../assets/australia.jpg";
 import thai from "../assets/thailand.jpg";
+import { Fade } from "react-awesome-reveal";
 
 const Coutries = () => {
   return (
     <div className="flex flex-col items-center mt-20">
-      <h3 className="text-2xl font-semibold mt-5">Countries We offer</h3>
-      <h1 className="text-4xl mt-5 font-bold oswald">
-        Best Countries to Travel
-      </h1>
+      <Fade>
+        <h3 className="text-2xl font-semibold mt-5">Countries We offer</h3>
+      </Fade>
+
+      <Fade>
+        <h1 className="text-4xl mt-5 font-bold oswald">
+          Best Countries to Travel
+        </h1>
+      </Fade>
 
       <div className="flex items-center gap-5 mt-5 text-xl font-semibold mb-10">
         <h4>Education</h4>
         <h4>Immigration</h4>
         <h4>Business</h4>
       </div>
+      <Fade duration={2000}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <img src={beijing} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">China</p>
+          </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <div>
-          <img src={beijing} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">China</p>
-        </div>
+          <div>
+            <img src={america} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">USA</p>
+          </div>
 
-        <div>
-          <img src={america} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">USA</p>
-        </div>
+          <div>
+            <img src={berlin} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">Germany</p>
+          </div>
 
-        <div>
-          <img src={berlin} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">Germany</p>
-        </div>
+          <div>
+            <img src={spain} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">Spain</p>
+          </div>
 
-        <div>
-          <img src={spain} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">Spain</p>
+          <div>
+            <img src={india} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">India</p>
+          </div>
+          <div>
+            <img src={uk} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">United Kingdom</p>
+          </div>
+          <div>
+            <img src={aus} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">Australia</p>
+          </div>
+          <div>
+            <img src={thai} className="h-[600px]" alt="" />
+            <p className="text-center font-semibold text-3xl">Thailand</p>
+          </div>
         </div>
-
-        <div>
-          <img src={india} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">India</p>
-        </div>
-        <div>
-          <img src={uk} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">United Kingdom</p>
-        </div>
-        <div>
-          <img src={aus} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">Australia</p>
-        </div>
-        <div>
-          <img src={thai} className="h-[600px]" alt="" />
-          <p className="text-center font-semibold text-3xl">Thailand</p>
-        </div>
-      </div>
+      </Fade>
     </div>
   );
 };
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,106 +1,111 @@
 import React from "react";
 import flags from "../assets/flags.jpg";
+import { Slide } from "react-awesome-reveal";
 
 const Faq = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 mt-32 gap-10  pb-10 ">
-      <div>
-        <h1 className="text-center text-3xl font-bold oswald">
-          Common questions answered
-        </h1>
+      <Slide direction="left">
+        <div>
+          <h1 className="text-center text-3xl font-bold oswald">
+            Common questions answered
+          </h1>
 
-        <h4 className="text-xl mt-5 text-center font-semibold mb-10">
-          At the heart of our commitment to providing exceptional immigration
-          solutions stands our trusted services.
-        </h4>
+          <h4 className="text-xl mt-5 text-center font-semibold mb-10">
+            At the heart of our commitment to providing exceptional immigration
+            solutions stands our trusted services.
+          </h4>
 
-        <img className="w-[400px] mx-auto opacity-70" src={flags} alt="" />
-      </div>
+          <img className="w-[400px] mx-auto opacity-70" src={flags} alt="" />
+        </div>
+      </Slide>
 
       {/* accordion */}
-      <div className="dark:bg-gray-900 dark:text-white rounded ">
-        <div className="collapse collapse-plus ">
-          <input type="radio" name="my-accordion-3" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            What services do you offer?
-          </div>
-          <div className="collapse-content">
-            <p className="mb-5">
-              We offer comprehensive immigration and visa consulting services,
-              including visa application assistance, document preparation,
-            </p>
+      <Slide direction="right">
+        <div className="dark:bg-gray-900 dark:text-white rounded ">
+          <div className="collapse collapse-plus ">
+            <input type="radio" name="my-accordion-3" defaultChecked />
+            <div className="collapse-title text-xl font-medium">
+              What services do you offer?
+            </div>
+            <div className="collapse-content">
+              <p className="mb-5">
+                We offer comprehensive immigration and visa consulting services,
+                including visa application assistance, document preparation,
+              </p>
 
-            <li>Comprehensive Visa Assistance</li>
-            <li>Visa Category Expertise</li>
-            <li>Transparency and Communication</li>
+              <li>Comprehensive Visa Assistance</li>
+              <li>Visa Category Expertise</li>
+              <li>Transparency and Communication</li>
+            </div>
+          </div>
+          <div className="collapse collapse-plus ">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              What is the consultation process like?
+            </div>
+            <div className="collapse-content">
+              <p>
+                We offer comprehensive immigration and visa consulting services,
+                including visa application assistance, document preparation,
+              </p>
+
+              <li>Comprehensive Visa Assistance</li>
+              <li>Visa Category Expertise</li>
+              <li>Transparency and Communication</li>
+            </div>
+          </div>
+          <div className="collapse collapse-plus ">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              How much do your services cost?
+            </div>
+            <div className="collapse-content">
+              <p>
+                We offer comprehensive immigration and visa consulting services,
+                including visa application assistance, document preparation, Our
+                cost are between 1000 to 5000 USD.
+              </p>
+            </div>
+          </div>
+
+          <div className="collapse collapse-plus ">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              How do I get started with your services?
+            </div>
+            <div className="collapse-content">
+              <p>
+                We offer comprehensive immigration and visa consulting services,
+                including visa application assistance. You can get started by
+                checking out our visa types and application details
+              </p>
+
+              <li>Checkout our visa categories</li>
+              <li>See visa requirement details</li>
+              <li>Apply for a visa</li>
+            </div>
+          </div>
+
+          <div className="collapse collapse-plus ">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title text-xl font-medium">
+              What is your success rate with visa applications?
+            </div>
+            <div className="collapse-content">
+              <p>
+                We offer comprehensive immigration and visa consulting services,
+                including visa application assistance. You can get started by
+                checking out our visa types and application details
+              </p>
+
+              <li>More than 5 visa categories</li>
+              <li>10+ countries</li>
+              <li>100% success rate</li>
+            </div>
           </div>
         </div>
-        <div className="collapse collapse-plus ">
-          <input type="radio" name="my-accordion-3" />
-          <div className="collapse-title text-xl font-medium">
-            What is the consultation process like?
-          </div>
-          <div className="collapse-content">
-            <p>
-              We offer comprehensive immigration and visa consulting services,
-              including visa application assistance, document preparation,
-            </p>
-
-            <li>Comprehensive Visa Assistance</li>
-            <li>Visa Category Expertise</li>
-            <li>Transparency and Communication</li>
-          </div>
-        </div>
-        <div className="collapse collapse-plus ">
-          <input type="radio" name="my-accordion-3" />
-          <div className="collapse-title text-xl font-medium">
-            How much do your services cost?
-          </div>
-          <div className="collapse-content">
-            <p>
-              We offer comprehensive immigration and visa consulting services,
-              including visa application assistance, document preparation, Our
-              cost are between 1000 to 5000 USD.
-            </p>
-          </div>
-        </div>
-
-        <div className="collapse collapse-plus ">
-          <input type="radio" name="my-accordion-3" />
-          <div className="collapse-title text-xl font-medium">
-            How do I get started with your services?
-          </div>
-          <div className="collapse-content">
-            <p>
-              We offer comprehensive immigration and visa consulting services,
-              including visa application assistance. You can get started by
-              checking out our visa types and application details
-            </p>
-
-            <li>Checkout our visa categories</li>
-            <li>See visa requirement details</li>
-            <li>Apply for a visa</li>
-          </div>
-        </div>
-
-        <div className="collapse collapse-plus ">
-          <input type="radio" name="my-accordion-3" />
-          <div className="collapse-title text-xl font-medium">
-            What is your success rate with visa applications?
-          </div>
-          <div className="collapse-content">
-            <p>
-              We offer comprehensive immigration and visa consulting services,
-              including visa application assistance. You can get started by
-              checking out our visa types and application details
-            </p>
-
-            <li>More than 5 visa categories</li>
-            <li>10+ countries</li>
-            <li>100% success rate</li>
-          </div>
-        </div>
-      </div>
+      </Slide>
     </div>
   );
 };
